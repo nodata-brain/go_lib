9a4443eb1f6bec564fca5d9dbe12528bf463f68b
nodata-brain <nodata.eng@gmail.com>
2019-06-25 18:41:09 +0900

    feat: add baseball_score

--- scraping/scraping.go
+++ scraping/scraping.go
@@ -1,20 +1,49 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"time"
+
 	"github.com/PuerkitoBio/goquery"
 )
 
-func GetPage(url string) {
+func GetPage(url string) []string {
 	doc, _ := goquery.NewDocument(url)
-	doc.Find("").Each(func(i int, s *goquery.Selection) {
-		text := s.Find("").Text()
-		fmt.Printf(text)
+	var text []string
+	a := doc.Find("* > section#contentsArea")
 
-	})
+	fmt.Println(a.Find("div.scoreBox").Text())
+	text = []string{"tes"}
+
+	return text
+}
+
+//type baseball
+func baseball() []string {
+
+	t := time.Now()
+	fmt.Println(t)
+	url := "https://www.nikkansports.com/baseball/professional/score/2019/pf-score-20190624.html"
+	result := GetPage(url)
+
+	return result
+
 }
 
 func main() {
-	url := ""
-	GetPage(url)
+
+	//read commandline option
+	media := flag.String("m", "baseball", "media_type")
+	flag.Parse()
+
+	var text []string
+	//select media type
+	if *media == "baseball" {
+		text = baseball()
+	}
+
+	//output
+	fmt.Println(text)
+
 }
